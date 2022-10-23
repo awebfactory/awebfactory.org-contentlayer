2545436d8c42060f6500b0f0db0d5e5919df6a77
Victor Kane <victorkane@gmail.com>
2022-10-23 10:07:21 -0300

    fix(unsuccessful): impede needless repetition of document type name in computed field url

---
+++ content/projects/pft.md
@@ -0,0 +1,16 @@
+---
+title: Project Flow & Tracker
+type: Project
+id: pft
+start_date: 2011-02-07
+end_date: 2022-11-22
+description: >
+  Project Flow & Tracker will run on AWebFactory.org as an open community #BuildInPublic and #LearnInPublic resource based on an ongoing Lean/UX and Agile process of adding value, prototyping, and testing. Triggering ongoing MVP pivoting and extensibility
+
+# team:
+#   lead: Victor Kane
+#   ux-ui: Lewis Armstrong
+#   dev: Mary James
+#   dev-ops: Sally Campos
+#   owner: Joan Clark
+---
--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,7 +2,6 @@ import { defineDocumentType, makeSource } from "contentlayer/source-files"
 
 const Post = defineDocumentType(() => ({
   name: "Post",
-  filePathPattern: `/**/*.md`,
   fields: {
     title: {
       type: "string",
@@ -17,13 +16,51 @@ const Post = defineDocumentType(() => ({
   },
   computedFields: {
     url: {
-      type: "string",
+      type: "json",
       resolve: (doc) => `/posts/${doc._raw.flattenedPath}`,
     },
   },
 }))
 
+const Project = defineDocumentType(() => ({
+  name: "Project",
+  filePathPattern: `/**/projects/*.md`,
+  fields: {
+    title: {
+      type: "string",
+      description: "The name of the project",
+      required: true,
+    },
+    id: {
+      type: "string",
+      description: "The id",
+      required: true,
+    },
+    start_date: {
+      type: "date",
+      description: "The starting date of the post",
+      required: false,
+    },
+    end_date: {
+      type: "date",
+      description: "The ending date of the post",
+      required: false,
+    },
+    description: {
+      type: "string",
+      description: "The description",
+      required: false,
+    },
+  },
+  computedFields: {
+    url: {
+      type: "json",
+      resolve: (doc) => `/projects/${doc._raw.flattenedPath}`,
+    },
+  },
+}))
+
 export default makeSource({
-  contentDirPath: "content/posts",
-  documentTypes: [Post],
+  contentDirPath: "content",
+  documentTypes: [Post, Project],
 })
